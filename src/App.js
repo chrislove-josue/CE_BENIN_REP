6330c169340e633828bda02b39e45f2794d88ef9
Chrislove Nestor <chrislovenestor@gmail.com>
2025-09-01 18:29:31 +0100

    ajout de analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AvatarEditor from "./AvatarEditor";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className="App">
       <h1 style={{ textAlign: "center" }}>Création d'avatar personnalisé</h1>
       <AvatarEditor />
+      <Analytics/>
     </div>
   );
 }
