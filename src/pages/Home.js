0f02e01e997b7b1ca94c3e16eab7015d2f69054f
Chrislove Nestor <chrislovenestor@gmail.com>
2025-09-03 10:43:22 +0100

    revenir en arrière avec 3 pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,6 @@ const Home = () => (
   <main className="home">
     <h1>Bienvenue à la Conférence de Merveilles</h1>
     <p>Vivez une expérience transformationnelle avec Rhapsodie des Réalités.</p>
-
-    <p>
-      La  Conference des Merveiles de le Rhapsodie des Réalités est un moment de célébration, d'enseignement et de transformation.
-      Rejoignez des milliers de participants autour du monde dans une atmosphère de foi et de miracles.
-    </p>
-
     <img
       src="/flyer.jpg"
       alt="Conférence"
